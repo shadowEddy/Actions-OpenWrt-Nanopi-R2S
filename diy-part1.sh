34a43bf2502d44822ffe512b4c128c860251017e
shadowEddy <73350258+shadowEddy@users.noreply.github.com>
2020-11-27 16:08:21 +0800

    Remove official theme argon and add jerrykuku's

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,7 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+#Remove official theme argon and add jerrykuku/luci-theme-argon
+rm -rf package/lean/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean
